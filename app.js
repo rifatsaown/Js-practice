03953d4d11178f672aafa5fea7b3ce6248fc8d09
rifatsaown <rifatsaown0@gmail.com>
2021-08-18 02:14:24 +0600

    Specal Math Done

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ var number2 = 0.2;
 var total =  number1 + number2;
 console.log(total);
 
-// fix it 
+// fix it 1
 var number1 = 0.1;
 var number2 = 0.2;
 var total =  number1 + number2;
 total = total.toFixed(1);
 total = parseFloat(total);
 console.log(total);
+
+// fix it 2
+var number1 = 0.1;
+var number2 = 0.2;
+var total =  number1 + number2;
+console.log(parseFloat(total.toFixed(1)));

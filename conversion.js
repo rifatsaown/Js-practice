f6e1d6d19145a798b52fa0af9932116541d724dd
rifatsaown <rifatsaown0@gmail.com>
2021-08-18 01:58:08 +0600

    Conversion file add

---
+++ conversion.js
@@ -0,0 +1,24 @@
+// String Sum 
+var name1 = "Md. Rifat";
+var name2 = "Hossen"
+var name3 = "Saown";
+var fullName = name1+" "+name2+" "+name3;
+console.log(fullName);
+
+// convert string to float number 
+var mathMark = "49.83";
+var physicsMark = "48.47";
+var mathMarkNumber = parseFloat(mathMark);
+var physicsMarkNumber = parseFloat(physicsMark);
+var totalMark = mathMarkNumber + physicsMarkNumber;
+var totalMark2 = "Total Mark : "+totalMark 
+console.log(totalMark2);
+
+// convert string to integer number
+var rifAge = "18";
+var ratAge = "17";
+var rifAgeNumber = parseInt(rifAge);
+var ratAgeNumber = parseInt(ratAge);
+var totalAge =  rifAgeNumber + ratAgeNumber;
+var totalAge2 = "Total Age : "+totalAge;
+console.log(totalAge2);

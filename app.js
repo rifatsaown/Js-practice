7ef6af150f044822cf58d49fcb045f224984635f
rifatsaown <rifatsaown0@gmail.com>
2021-08-18 02:11:56 +0600

    Conversion file add

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-var age = 10;
-age *=20
-console.log(age);
+// speacal Sum
+var number1 = 0.1;
+var number2 = 0.2;
+var total =  number1 + number2;
+console.log(total);
+
+// fix it 
+var number1 = 0.1;
+var number2 = 0.2;
+var total =  number1 + number2;
+total = total.toFixed(1);
+total = parseFloat(total);
+console.log(total);

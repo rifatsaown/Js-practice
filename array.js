29f6158f0175c3517f68b0e17f9bf80eb8a0ccb8
rifatsaown <rifatsaown0@gmail.com>
2021-08-18 02:49:39 +0600

    array added

---
+++ array.js
@@ -0,0 +1,21 @@
+// array number
+var number = [50,20,30,50,100,30,50,100,30,50,100,50,100,45,30];
+console.log(number);
+
+// array string 
+var name =["Rifat", "Saown", "Rat", "Rif"];
+console.log(name);
+
+// array length
+var age = [50,20,30,50,100];
+var ageLength = age.length;
+console.log(ageLength);
+
+//array length 2
+var age = [50,20,30,50,100,30,50,100,30,50,100,50,100,45,30];
+console.log(age.length);
+
+// array length string
+var name =["Rifat", "Saown", "Rat", "Rif"];
+var nameLength = name.length;
+console.log(nameLength);

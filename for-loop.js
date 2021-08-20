d27dacdae39df29997c598b48408befa6537d6a6
rifatsaown <rifatsaown0@gmail.com>
2021-08-20 16:32:54 +0600

    loop added

---
+++ for-loop.js
@@ -0,0 +1,26 @@
+for(var rostPaiche = 1; rostPaiche <=10; rostPaiche++) {
+    console.log("Diya disi" , rostPaiche);
+}
+
+// number
+for(var i = 1; i <= 10 ; i++) {
+    console.log(i);
+}
+
+
+// odd
+for (var i = 1; i <= 10 ; i+=2) {
+    console.log(i);
+}
+
+// even
+for (var i = 2; i <= 10 ; i+=2) {
+    console.log(i);
+}
+
+// array element 
+var number = [45,55,65,68,69,47,75,77,87,79];
+for(var i = 0; i < number.length; i++) {
+    var element = number[i];
+    console.log(element);
+}
---
+++ while-loop.js
@@ -0,0 +1,18 @@
+var rostPaiche = 1;
+
+while (rostPaiche <= 10) {
+    console.log("Rost daua hoiche", rostPaiche);
+    rostPaiche++ ;
+}
+
+var evenNumber = 2;
+while (evenNumber <= 20) {
+    console.log(evenNumber);
+    evenNumber+= 2;
+}
+
+var evenNumber = 1;
+while (evenNumber <= 20) {
+    console.log(evenNumber);
+    evenNumber+= 2;
+}
